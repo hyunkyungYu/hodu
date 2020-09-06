c4c4d21d7fc54bde983c2c97363c08887c12e389
이지윤 <82106@DESKTOP-QSLHFQ4>
2020-09-06 17:55:54 +0900

    이지윤 입니다

--- Hello2/Homework/homeWork2.java
+++ Hello2/Homework/homeWork2.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class homeWork2 {
 	public static void main(String[] args) {
+		System.out.println("¾ßÈ£");
 		Scanner a = new Scanner(System.in);
 		int A = 0;
 		int B = 0;
